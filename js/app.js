22a4371d14ed1efd416aa43ef4dc2b9d973a46e7
FrederickFoxxx <fwhduffield@gmail.com>
2017-07-02 00:09:13 +0100

    basic layout css and html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('hello fred');
+
+
+// DOM UP-IN-ERE - LISTENING EVENTS ON BUTTONS
+// START BUTTON
+// on click run question GENERATOR
+
+// QUESTION GENERATOR
+// some kinda loop? to make inputs taken from variable defined outside
+// animated pop options
+// on click run COUNTER
+
+//COUNTER
+// for A add 1 / for B add 5 / for C add 10
+
+// INTERVAl
+// after 3 seconds and sprite animation run question Generator
+
+
+// EVALUATION LOGIC
+// madness? make the result complete random ?
+// if score < number run whatever conclusion
+// if score > number run
