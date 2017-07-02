9090e8aabc97d07b094ca8d8fe18073395e7543b
FrederickFoxxx <fwhduffield@gmail.com>
2017-07-02 16:16:18 +0100

    some work on random item generator

--- js/app.js
+++ js/app.js
@@ -6,4 +6,27 @@ console.log('hello fred');
 // the 2 objects in play are evaluated
 // the evaluation effects the life and power bars
 // the game ends when one player life bar(-3) is empty or the other players power bar(3) is full
- 
+
+const items = ['Chicken', 'Mushroom', 'Beer', 'syringe', 'fire', 'Water' ];
+//Start button
+// function createStartButton (i) {
+// // document.newButton = $('<button/>');
+// // return this.newButton;
+// // on click run itemDistributor fuction
+// // for both computer and human
+// }
+
+$(init);
+function init(){
+  $('.powerupBox').each(itemDistributor());
+}
+//item distributor
+// a random generator of objects
+function itemDistributor(){
+  const randomSelectionOfItems = items[Math.floor(Math.random()*items.length)];
+  $('.powerupBox').append(randomSelectionOfItems);
+  console.log(randomSelectionOfItems);
+}
+
+
+// $('.powerupBox').on('click', function (){});
